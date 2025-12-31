6a4ebca75a7a1d5a4d180591308964a70a17e6f2
Angel Suarez <112484606+AngelBlackBlue@users.noreply.github.com>
2025-12-31 18:53:28 -0300

    add test basic

--- spec/prueba.spec.js
+++ spec/prueba.spec.js
@@ -1,5 +1,46 @@
-describe("demo", function () {
-  it("Este test debe pasar siempre", function () {
-    expect(4 + 2).toBe(6);
+const ToDoList = require("../scripts/index");
+
+describe("The ToDoList class", () => {
+  it("should be a class", () => {
+    expect(typeof ToDoList.prototype.constructor).toBe("function");
   });
+
+  it("should have a method getTasks", () => {
+    const toDoList = new ToDoList();
+    expect(toDoList.getTasks).toBeDefined();
+  });
+
+  it("should have a method addTask", () => {
+    const toDoList = new ToDoList();
+    expect(toDoList.addTask).toBeDefined();
+  });
+
+  it("should have a method removeTask", () => {
+    const toDoList = new ToDoList();
+    expect(toDoList.removeTask).toBeDefined();
+  });
+
+  it("The getTasks method should return an array", () => {
+    const toDoList = new ToDoList();
+    expect(Array.isArray(toDoList.getTasks())).toBe(true);
+  });
+
+  it("The addTask method should add a task to the array", () => {
+    const toDoList = new ToDoList();
+    toDoList.addTask("task");
+    expect(toDoList.getTasks()).toContain("task");
+  });
+
+  it("The removeTask method should remove a task from the array", () => {
+    const toDoList = new ToDoList();
+    toDoList.addTask("task");
+    toDoList.removeTask("task");
+    expect(toDoList.getTasks()).not.toContain("task");
+  });
+
+  
 });
+
+// npx jasmine
+// npx jasmine-browser-runner init --esm
+
